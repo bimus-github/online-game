2022c3fb2aecb641834a5940dc2acb11617b1e3d
bimus-github <bimus2022@gmail.com>
2023-08-31 19:55:47 +0500

    adding modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,33 @@
+import { useRef } from "react";
+
+function Modal({
+  isModalOpen,
+  closeModal,
+  children,
+}: {
+  isModalOpen: boolean;
+  closeModal: () => void;
+  children: JSX.Element;
+}) {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const handleOutsideClick = (event: React.MouseEvent<HTMLElement>) => {
+    if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+      closeModal();
+    }
+  };
+  if (!isModalOpen) return null;
+
+  return (
+    <div
+      onClick={handleOutsideClick}
+      className="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center"
+    >
+      <div ref={modalRef} className="bg-white w-96 rounded-lg p-6">
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
