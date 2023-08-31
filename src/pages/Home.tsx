c6e14237f7a50835e0ce6ffba66bccb451e944d4
bimus-github <bimus2022@gmail.com>
2023-08-31 20:37:14 +0500

    connecting create new room button to store

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
 import Modal from "../components/Modal";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { roomActions } from "../store/features/room";
+import { ERROR_ENUM, Room_Type } from "../type";
 
 function Home() {
+  const dispatch = useAppDispatch();
+  const rooms = useAppSelector((state) => state.room);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const [roomName, setRoomName] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<ERROR_ENUM>(ERROR_ENUM.NONE);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -11,30 +21,65 @@ function Home() {
   const closeModal = () => {
     setIsModalOpen(false);
   };
+
+  const handleCreateNewRoom = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setError(ERROR_ENUM.NONE);
+    if (roomName.length === 0) return setError(ERROR_ENUM.NAME);
+    const date = new Date();
+
+    const newRoom: Room_Type = {
+      id: `${roomName}${password}`,
+      name: roomName,
+      description,
+      password,
+      date,
+    };
+
+    dispatch(roomActions.createRoom(newRoom));
+    setError(ERROR_ENUM.NONE);
+    setRoomName("");
+    setDescription("");
+    setPassword("");
+    setIsModalOpen(false);
+  };
   return (
     <div className={styles.main}>
       <button onClick={openModal} className={styles.generateRoomBtn}>
         <p>Add room</p>
       </button>
       <div className={styles.roomListsDiv}>
-        {["", "", "", "", "", "", "", ""].map((_, key) => (
-          <div className={styles.infoRoom} key={key}>
-            <p>{key + 1}. Room Name</p>
-            <p>Description</p>
-            <p>lock</p>
-          </div>
-        ))}
+        {rooms.length !== 0 ? (
+          rooms.map((_, key) => (
+            <div className={styles.infoRoom} key={key}>
+              <p>{key + 1}. Room Name</p>
+              <p></p>
+              <p>lock</p>
+            </div>
+          ))
+        ) : (
+          <div>there is no rooms, yet!</div>
+        )}
       </div>
 
       {/* generate room modal */}
       <Modal closeModal={closeModal} isModalOpen={isModalOpen}>
         <div className={styles.modal}>
           <div className={styles.modalTitle}>Create Room</div>
-          <form action="" className={styles.modalForm}>
+          <form
+            onSubmit={handleCreateNewRoom}
+            action=""
+            className={styles.modalForm}
+          >
             <div className={styles.modalInputDiv}>
               <p className={styles.inputTitle}>Room Name</p>
               <input
-                className={styles.modalInput}
+                value={roomName}
+                onChange={(e) => setRoomName(e.target.value)}
+                className={`${styles.modalInput} ${
+                  error === ERROR_ENUM.NAME ? "border-red-400" : ""
+                }`}
                 type="text"
                 placeholder="eg: Game with Muhammad"
               />
@@ -42,6 +87,8 @@ function Home() {
             <div className={styles.modalInputDiv}>
               <p className={styles.inputTitle}>Description</p>
               <input
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
                 className={styles.modalInput}
                 type="text"
                 placeholder="eg: Please, do not disturb"
@@ -50,12 +97,19 @@ function Home() {
             <div className={styles.modalInputDiv}>
               <p className={styles.inputTitle}>Password</p>
               <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 className={styles.modalInput}
                 type="text"
-                placeholder="***"
+                placeholder="eg: 123**"
               />
             </div>
-            <button className={styles.createRoomBtn}>Create</button>
+            <button type="submit" className={styles.createRoomBtn}>
+              Create
+            </button>
+            {error === ERROR_ENUM.NAME && (
+              <p className="text-red-400">Pleas, create room name!</p>
+            )}
           </form>
         </div>
       </Modal>
@@ -70,7 +124,7 @@ const styles = {
   generateRoomBtn:
     "absolute top-[5%] right-[5%] h-[50px] bg-slate-400 hover:bg-slate-300 text-[22px] pl-2 pr-4 rounded shadow-md",
   roomListsDiv:
-    "w-[60%] bg-slate-400 rounded-md p-4 flex flex-col gap-4 shadow-lg overflow-y-auto max-h-[70%]",
+    "w-[60%] bg-slate-400 rounded-md p-4 flex flex-col gap-4 shadow-lg overflow-y-auto h-[70%]",
   infoRoom:
     " w-full  bg-slate-200 hover:bg-slate-100 p-4 rounded-md shadow-sm flex flex-row justify-around gap-5",
   modal: "relative w-full ",
--- src/type/index.ts
+++ src/type/index.ts
@@ -1,6 +1,12 @@
 export interface Room_Type {
   id: string;
   name: string;
-  descripion: string;
+  description: string;
   password: string;
+  date: Date;
+}
+
+export enum ERROR_ENUM {
+  NONE = 0,
+  NAME = 1,
 }
